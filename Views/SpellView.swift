291c16c607fd8564c229f2c111b1df028de9622f
Elijah K <elijah.khamo01@utrgv.edu>
2025-11-02 12:41:06 -0600

    Added spell view. Includes spell name and a caption of the description

---
+++ Views/SpellView.swift
@@ -0,0 +1,18 @@
+//
+//  SpellView.swift
+//  HarryPotterSwiftUIApp
+//
+//  Created by Elijah K on 11/2/25.
+//
+
+import SwiftUI
+
+struct SpellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpellView()
+}
